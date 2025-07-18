232e2d5eb3df515fd3690d2b3d1789f71c2011c2
Gleb <gleb-01.ru@mail.ru>
2025-07-19 01:35:16 +0300

    upd(auth): add permission_classes

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,12 @@
 from rest_framework import  viewsets
 from .models import Habit, Goal, HabitAction, GoalProgress
 from .serializers import HabitSerializer, GoalSerializer, HabitActionSerializer, GoalProgressSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
@@ -15,6 +17,7 @@ class HabitViewSet(viewsets.ModelViewSet):
 class GoalViewSet(viewsets.ModelViewSet):
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
@@ -25,6 +28,7 @@ class GoalViewSet(viewsets.ModelViewSet):
 class HabitActionViewSet(viewsets.ModelViewSet):
     queryset = HabitAction.objects.all()
     serializer_class = HabitActionSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
@@ -35,6 +39,7 @@ class HabitActionViewSet(viewsets.ModelViewSet):
 class GoalProgressViewSet(viewsets.ModelViewSet):
     queryset = GoalProgress.objects.all()
     serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
