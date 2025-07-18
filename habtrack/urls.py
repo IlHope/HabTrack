8fc3f91b24f42f0a4d5de9814d24182c4701a99c
Gleb <gleb-01.ru@mail.ru>
2025-07-19 01:27:42 +0300

    feat(auth): add authorization via JWT

--- habtrack/urls.py
+++ habtrack/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tracker.urls')),  # подключаем API
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # логин: получаем access и refresh
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   # обновляем access по refresh
 ]
